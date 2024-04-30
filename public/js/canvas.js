75643553f80c191f40ab27423e3519f41b284766
tadjh <tadjh.brooks@gmail.com>
2024-04-30 09:29:53 -0700

    Init Commit

---
+++ public/js/canvas.js
@@ -0,0 +1,120 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const font = urlParams.get("font") || "Permanent Marker";
+const text = urlParams.get("text") || "Hello World";
+const fontColor = urlParams.get("font_color") || "252937";
+const fontSize = Number(urlParams.get("font_size") || "60");
+const width = Number(urlParams.get("width") || "512");
+const height = Number(urlParams.get("height") || "512");
+const offsetY = Number(urlParams.get("offsetY") || "256");
+const offsetX = Number(urlParams.get("offsetX") || "256");
+const anchor = urlParams.get("anchor") || "mm";
+
+const canvas = document.createElement("canvas");
+canvas.style.width = width + "px";
+canvas.style.height = height + "px";
+
+window.devicePixelRatio = 2;
+
+const retinaWidth = width * window.devicePixelRatio;
+const retinaHeight = height * window.devicePixelRatio;
+const retinaFontSize = fontSize * window.devicePixelRatio;
+
+canvas.setAttribute("width", retinaWidth);
+canvas.setAttribute("height", retinaHeight);
+
+document.body.appendChild(canvas);
+
+window.addEventListener("load", draw);
+
+function draw() {
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.addEventListener("load", () => {
+      ctx.drawImage(img, 0, 0, retinaWidth, retinaHeight);
+    });
+    img.src = "./img/parchment_sign_1_bg.png";
+
+    const textDisplay = decodeURIComponent(text);
+
+    function drawText(retinaFontSize, recursions = 0) {
+      if (recursions > 3) ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.font = retinaFontSize + "px " + decodeURIComponent(font);
+      ctx.fillStyle = "#" + fontColor;
+      let textAlign = "";
+      let textBaseline = "";
+
+      switch (anchor[0]) {
+        case "r":
+          textAlign = "end";
+          break;
+        case "l":
+          textAlign = "start";
+          break;
+        case "m":
+        default:
+          textAlign = "center";
+          break;
+      }
+
+      switch (anchor[1]) {
+        case "a":
+          textBaseline = "top";
+          break;
+        case "t":
+          textBaseline = "hanging";
+          break;
+        case "b":
+          textBaseline = "ideographic";
+          break;
+        case "d":
+          textBaseline = "bottom";
+          break;
+        case "s":
+          textBaseline = "alphabetic";
+          break;
+        case "m":
+        default:
+          textBaseline = "middle";
+          break;
+      }
+
+      ctx.textBaseline = textBaseline;
+      ctx.textAlign = textAlign;
+
+      const { width: textWidth } = ctx.measureText(textDisplay);
+
+      const ratio = (retinaWidth * 0.9) / textWidth;
+
+      if (ratio < 1) {
+        console.log(ratio);
+        return drawText(retinaFontSize * ratio, recursions + 1);
+      }
+
+      return ctx.fillText(
+        textDisplay,
+        offsetX * window.devicePixelRatio,
+        offsetY * window.devicePixelRatio
+      );
+    }
+
+    WebFontConfig = {
+      google: {
+        families: [font],
+      },
+      active: () => drawText(retinaFontSize),
+    };
+
+    return (function (d) {
+      var wf = d.createElement("script"),
+        s = d.scripts[0];
+      wf.src =
+        "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+      wf.async = true;
+      s.parentNode.insertBefore(wf, s);
+    })(document);
+  }
+}
---
+++ public/js/default.js
@@ -0,0 +1,102 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const font = urlParams.get("font") || "Permanent Marker";
+const text = urlParams.get("text") || "Hello World";
+const fontColor = urlParams.get("font_color") || "252937";
+const fontSize = Number(urlParams.get("font_size") || "60");
+const width = urlParams.get("width") || "512";
+const height = urlParams.get("height") || "512";
+const offsetX = urlParams.get("offsetX") || "256";
+const offsetY = urlParams.get("offsetY") || "256";
+const anchor = urlParams.get("anchor") || "mm";
+
+window.addEventListener("load", draw);
+
+function draw() {
+  const root = document.createElement("div");
+  const node = document.createElement("div");
+
+  document.body.appendChild(root);
+  root.style.width = width + "px";
+  root.style.height = height + "px";
+  root.style.backgroundImage = "url(img/parchment_sign_1_bg.png)";
+  root.style.position = "relative";
+
+  node.style.fontFamily = decodeURIComponent(font);
+  const textNode = document.createTextNode(decodeURIComponent(text));
+  node.appendChild(textNode);
+  node.style.color = "#" + fontColor;
+  node.style.fontSize = fontSize + "px";
+  node.style.position = "relative";
+  node.style.width = "max-content";
+  node.style.lineHeight = "1";
+  node.style.opacity = "0";
+
+  root.appendChild(node);
+
+  node.style.left = offsetX + "px";
+  node.style.top = offsetY + "px";
+
+  function anchorText(recursions = 0) {
+    if (recursions > 3) return;
+    const { width: textWidth, height: textHeight } =
+      node.getBoundingClientRect();
+
+    const ratio = (width * 0.9) / textWidth;
+
+    if (ratio < 1) {
+      node.style.fontSize = fontSize * ratio + "px";
+      return anchorText(recursions + 1);
+    }
+
+    let translateX = "0px";
+    let translateY = "0px";
+
+    switch (anchor[0]) {
+      case "m":
+        translateX = `-${textWidth / 2}px`;
+        break;
+      case "r":
+        translateX = `-${textWidth}px`;
+        break;
+      case "l":
+      default:
+        break;
+    }
+
+    switch (anchor[1]) {
+      case "m":
+        translateY = `-${textHeight / 2}px`;
+        break;
+      case "a":
+      case "t":
+      case "s":
+      case "b":
+      case "d":
+      default:
+        console.log(
+          "Ascender, Top, Baseline, Bottom & Descender vertical anchor point is not implemented."
+        );
+        break;
+    }
+
+    node.style.transform = `translate(${translateX},${translateY})`;
+    node.classList.add("fade-in");
+    node.style.removeProperty("opacity");
+  }
+
+  WebFontConfig = {
+    google: {
+      families: [font],
+    },
+    active: anchorText,
+  };
+
+  return (function (d) {
+    var wf = d.createElement("script"),
+      s = d.scripts[0];
+    wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+  })(document);
+}
